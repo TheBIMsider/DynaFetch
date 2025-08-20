1e438ddb7c65d967910bb81b7993a2325788d7fc
Carl Storms <37883693+TheBIMsider@users.noreply.github.com>
2025-08-20 17:38:41 -0600

    Update documentation with Session 8 changes

--- src/DynaFetch/obj/Debug/net8.0/DynaFetch.AssemblyInfo.cs
+++ src/DynaFetch/obj/Debug/net8.0/DynaFetch.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Modern REST API package for Dynamo 3.0 - inspired by DynaWeb")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60ab1b713255e833269c9e5b035f4cf0588c2737")]
 [assembly: System.Reflection.AssemblyProductAttribute("DynaFetch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DynaFetch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
