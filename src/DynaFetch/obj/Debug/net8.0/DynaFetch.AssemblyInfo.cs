16bd194aaac98a15e07d44741af48076da211ee9
Carl Storms <37883693+TheBIMsider@users.noreply.github.com>
2025-09-08 18:38:28 -0600

    Updates to Landing page

--- src/DynaFetch/obj/Debug/net8.0/DynaFetch.AssemblyInfo.cs
+++ src/DynaFetch/obj/Debug/net8.0/DynaFetch.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Modern REST API package for Dynamo 3.0 - inspired by DynaWeb")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a8f6ad85e9dc22d25fee8cc82894d869dae15bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+747f2fed941a0006aeb1df6262731518b88edabc")]
 [assembly: System.Reflection.AssemblyProductAttribute("DynaFetch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DynaFetch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
